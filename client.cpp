92f8a4ba49bcdecc574c82bee613022ae01188ca
manzil.roy@gmail.com <manzil.roy@iais.fraunhofer.de>
2021-10-17 11:45:57 +0200

    client for ngrest server for tictactoe

---
+++ client.cpp
@@ -0,0 +1,242 @@
+#include <QtWidgets>
+#include <QtNetwork>
+
+#include "client.h"
+
+
+Client::Client(QWidget *parent)
+    : QDialog(parent)
+    , playerCombo(new QComboBox)
+    , gameCombo(new QComboBox)
+    , rowCombo(new QComboBox)
+    , colCombo(new QComboBox)
+    , gameBoards(new QScrollArea)
+    , CreatePlayer(new QPushButton(tr("Create new Player")))
+    , gameMove(new QPushButton(tr("Make A Move")))
+    , CreateNewGame(new QPushButton(tr("Create New Game")))
+    , pollingTimer(new QTimer())
+
+{
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    pollingTimer->setInterval(1000);
+
+    playerCombo->setEditable(false);
+    gameCombo->setEditable(false);
+    rowCombo->setEditable(false);
+    colCombo->setEditable(false);
+    rowCombo->addItem("1");
+    rowCombo->addItem("2");
+    rowCombo->addItem("3");
+    colCombo->addItem("1");
+    colCombo->addItem("2");
+    colCombo->addItem("3");
+
+    auto playerID = new QLabel(tr("&Player ID:"));
+    auto gameID = new QLabel(tr("&Game ID:"));
+    auto row_label = new QLabel(tr("&Row Index:"));
+    auto col_label = new QLabel(tr("&Column Index:"));
+    playerID->setBuddy(playerCombo);
+    gameID->setBuddy(gameCombo);
+    row_label->setBuddy(rowCombo);
+    col_label->setBuddy(colCombo);
+
+    statusLabel = new QLabel(tr("Responses will be displayed here."));
+    gameBoards->setEnabled(true);
+    gameMove->setDefault(true);
+    gameMove->setEnabled(true);
+
+    CreateNewGame->setDefault(true);
+    CreateNewGame->setEnabled(true);
+
+    CreatePlayer->setDefault(true);
+    CreatePlayer->setEnabled(true);
+
+    auto quitButton = new QPushButton(tr("Quit"));
+
+    auto buttonBox = new QDialogButtonBox;
+    buttonBox->addButton(CreatePlayer, QDialogButtonBox::ActionRole);
+    buttonBox->addButton(CreateNewGame, QDialogButtonBox::ActionRole);
+    buttonBox->addButton(gameMove, QDialogButtonBox::ActionRole);
+    buttonBox->addButton(quitButton, QDialogButtonBox::RejectRole);
+
+
+    addPlayer = new QNetworkAccessManager(this);
+    addGame = new QNetworkAccessManager(this);
+    PlayerFetcher = new QNetworkAccessManager(this);
+    GameFetcher = new QNetworkAccessManager(this);
+    connect(addPlayer, &QNetworkAccessManager::finished, this, &Client::response_addplayer);
+    connect(PlayerFetcher, &QNetworkAccessManager::finished, this, &Client::response_list_of_Players);
+    connect(addGame, &QNetworkAccessManager::finished, this, &Client::response_addgame);
+    connect(GameFetcher, &QNetworkAccessManager::finished, this, &Client::response_list_of_Games);
+    connect(pollingTimer, &QTimer::timeout, this, &Client::GetAllGames);
+    connect(pollingTimer, &QTimer::timeout, this, &Client::GetAllPlayers);
+
+
+
+    connect(gameMove, &QAbstractButton::clicked, this, &Client::makeMove);
+    connect(CreatePlayer, &QAbstractButton::clicked, this, &Client::CreatePlayerHandler);
+    connect(CreateNewGame, &QAbstractButton::clicked, this, &Client::CreateGameHander);
+    connect(quitButton, &QAbstractButton::clicked, this, &QWidget::close);
+
+    QGridLayout *mainLayout = nullptr;
+    if (QGuiApplication::styleHints()->showIsFullScreen() || QGuiApplication::styleHints()->showIsMaximized()) {
+        auto outerVerticalLayout = new QVBoxLayout(this);
+        outerVerticalLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Ignored, QSizePolicy::MinimumExpanding));
+        auto outerHorizontalLayout = new QHBoxLayout;
+        outerHorizontalLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::MinimumExpanding, QSizePolicy::Ignored));
+        auto groupBox = new QGroupBox(QGuiApplication::applicationDisplayName());
+        mainLayout = new QGridLayout(groupBox);
+        outerHorizontalLayout->addWidget(groupBox);
+        outerHorizontalLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::MinimumExpanding, QSizePolicy::Ignored));
+        outerVerticalLayout->addLayout(outerHorizontalLayout);
+        outerVerticalLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Ignored, QSizePolicy::MinimumExpanding));
+    } else {
+        mainLayout = new QGridLayout(this);
+    }
+    mainLayout->addWidget(playerID, 0, 0);
+    mainLayout->addWidget(playerCombo, 0, 1);
+    mainLayout->addWidget(gameID, 1, 0);
+    mainLayout->addWidget(gameCombo, 1, 1);
+    mainLayout->addWidget(row_label,2,0);
+    mainLayout->addWidget(rowCombo,2,1);
+    mainLayout->addWidget(col_label,3,0);
+    mainLayout->addWidget(colCombo,3,1);
+    mainLayout->addWidget(gameBoards, 5, 0, 3, 2);
+    mainLayout->addWidget(statusLabel, 27, 0, 3, 2);
+    mainLayout->addWidget(buttonBox, 30, 0, 1, 2);
+
+    setWindowTitle(QGuiApplication::applicationDisplayName());
+    pollingTimer->start();
+    //GetAllPlayers();
+    //GetAllGames();
+}
+
+QString Client::BoardCharacter(int asci_value)
+{
+    QString returnchar;
+    if(asci_value==46)
+        returnchar = QString("   ");
+    if(asci_value==88)
+        returnchar = QString("X");
+    if(asci_value==79)
+        returnchar = QString("O");
+    return returnchar;
+}
+
+
+/* APIs and handler to create players and fetch their responses */
+
+void Client::CreatePlayerHandler()
+{
+  QByteArray data= "";
+  QNetworkRequest request(QUrl("http://localhost:9098/tictacserver/newplayer"));
+  request.setRawHeader("Content-Type", "application/x-www-form-urlencoded");
+  addPlayer->post(request,data);
+}
+
+void Client::response_addplayer(QNetworkReply* reply)
+{
+  QByteArray response =  reply->readAll();
+  statusLabel->setText(QJsonDocument::fromJson(response).object().value("result").toString());
+  GetAllPlayers();
+}
+
+void Client::response_list_of_Players(QNetworkReply* reply)
+{
+    QByteArray response =  reply->readAll();
+    QJsonObject document = QJsonDocument::fromJson(response).object();
+    QJsonValue data = document.value("result");
+    //QJsonObject list_of_Players = data.toObject();
+    QStringList list_of_Players = data.toObject().keys();
+    int current = playerCombo->currentIndex();
+    int n = list_of_Players.length();
+    playerCombo->setEditable(false);
+    playerCombo->clear();
+    for(int i = 0; i<n;++i)
+      playerCombo->addItem(list_of_Players[i]);
+    playerCombo->setCurrentIndex(current);
+}
+
+void Client::GetAllPlayers()
+{
+    QByteArray data = "";
+    QNetworkRequest request(QUrl("http://localhost:9098/tictacserver/getplayerinfo"));
+    request.setRawHeader("Content-Type", "application/x-www-form-urlencoded");
+    PlayerFetcher->get(request);
+}
+
+
+
+/* APIs and handler to create games and fetch their responses */
+void Client::response_list_of_Games(QNetworkReply* reply)
+{
+    QString textgame = "List of Games Played :\n\n";
+    QByteArray response =  reply->readAll();
+    QJsonObject document = QJsonDocument::fromJson(response).object();
+    QJsonValue data = document.value("result");
+    QJsonObject gameMap = data.toObject();
+    QStringList list_of_games = gameMap.keys();
+    int current = gameCombo->currentIndex();
+    int n = list_of_games.length();
+    gameCombo->setEditable(false);
+    gameCombo->clear();
+    for(int i = 0; i<n;++i)
+    {
+      gameCombo->addItem(list_of_games[i]);
+      QJsonArray board = gameMap.value(list_of_games[i]).toArray();
+      QJsonArray r1 = board[0].toArray();
+      QJsonArray r2 = board[1].toArray();
+      QJsonArray r3 = board[2].toArray();
+      QString linemid("-------------");
+      QString line1 = BoardCharacter(r1[0].toInt()) + " | " +  BoardCharacter(r1[1].toInt()) + " | " +  BoardCharacter(r1[2].toInt());
+      QString line2 = BoardCharacter(r2[0].toInt()) + " | " +  BoardCharacter(r2[1].toInt()) + " | " +  BoardCharacter(r2[2].toInt());
+      QString line3 = BoardCharacter(r3[0].toInt()) + " | " +  BoardCharacter(r3[1].toInt()) + " | " +  BoardCharacter(r3[2].toInt());
+      QString thisgame = "~~~~~~~~~~~ Game ID : "+QString(list_of_games[i])+"\n"+line1+"\n"+linemid+"\n"+line2+"\n"+linemid+"\n"+line3+"\n";
+      textgame = textgame + thisgame+ "\n";
+    }
+    QWidget *central = new QWidget;
+    QVBoxLayout *layout = new QVBoxLayout(central);
+    layout->addWidget(new QLabel(textgame));
+    gameBoards->setWidget(central);
+    gameBoards->setWidgetResizable(true);
+    gameCombo->setCurrentIndex(current);
+}
+
+void Client::GetAllGames()
+{
+    QByteArray data = "";
+    QNetworkRequest request(QUrl("http://localhost:9098/tictacserver/getallgames"));
+    request.setRawHeader("Content-Type", "application/x-www-form-urlencoded");
+    GameFetcher->get(request);
+}
+
+void Client::response_addgame(QNetworkReply* reply)
+{
+  QByteArray response =  reply->readAll();
+  statusLabel->setText(QJsonDocument::fromJson(response).object().value("result").toString());
+  GetAllGames();
+}
+
+void Client::CreateGameHander()
+{
+  QByteArray data = "";
+  QNetworkRequest request(QUrl("http://localhost:9098/tictacserver/createnewgame"));
+  request.setRawHeader("Content-Type", "application/x-www-form-urlencoded");
+  addGame->post(request,data);
+}
+
+void Client::makeMove()
+{
+  QByteArray data = "";
+  QString api = "http://localhost:9098/tictacserver/makeamove";
+  QString player(playerCombo->currentText());
+  QString gameid(gameCombo->currentText());
+  QString rowIdx(rowCombo->currentText());
+  QString colIds(colCombo->currentText());
+  const QString full_api = api + "/" + gameid + "/" + player + "/" + rowIdx + "/" + colIds;
+  QUrl path(full_api);
+  QNetworkRequest request(path);
+  request.setRawHeader("Content-Type", "application/x-www-form-urlencoded");
+  addGame->post(request,data);
+}
+
---
+++ client.h
@@ -0,0 +1,54 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QDataStream>
+#include <QDialog>
+#include <QtNetwork>
+
+QT_BEGIN_NAMESPACE
+class QComboBox;
+class QLabel;
+class QPushButton;
+class QScrollArea;
+
+QT_END_NAMESPACE
+
+class Client : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Client(QWidget *parent = nullptr);
+
+private slots:
+    void CreatePlayerHandler();
+    void CreateGameHander();
+    void makeMove();
+
+    QString BoardCharacter(int);
+    void response_addplayer(QNetworkReply*);
+    void response_list_of_Players(QNetworkReply*);
+    void GetAllPlayers();
+
+    void response_addgame(QNetworkReply*);
+    void GetAllGames();
+    void response_list_of_Games(QNetworkReply*);
+
+private:
+    QComboBox *playerCombo = nullptr;
+    QComboBox *gameCombo = nullptr;
+    QComboBox *rowCombo = nullptr;
+    QComboBox *colCombo = nullptr;
+    QLabel *statusLabel = nullptr;
+    QScrollArea* gameBoards = nullptr;
+    QPushButton *gameMove = nullptr;
+    QPushButton *CreatePlayer = nullptr;
+    QPushButton *CreateNewGame = nullptr;
+
+    QNetworkAccessManager* addPlayer = nullptr;
+    QNetworkAccessManager* addGame = nullptr;
+    QNetworkAccessManager* PlayerFetcher = nullptr;
+    QNetworkAccessManager* GameFetcher = nullptr;
+    QTimer* pollingTimer = nullptr;    
+};
+#endif
